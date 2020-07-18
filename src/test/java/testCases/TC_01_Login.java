878f119ab9191ed9570f123cad0151176451b375
ankushingole <56147964+ankushingole@users.noreply.github.com>
2020-07-18 19:43:33 +0530

    second commit

--- src/test/java/testCases/TC_01_Login.java
+++ src/test/java/testCases/TC_01_Login.java
@@ -1,9 +1,39 @@
 package testCases;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TC_01_Login {
 
-	public static void main(String[] args) {
-		System.out.println("This is my first test case");
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+
+	@AfterMethod
+	public void quitBrowser() {
+
+		driver.quit();
+	}
+
+	@Test
+	public void login() {
+
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
 	}
 
 }
